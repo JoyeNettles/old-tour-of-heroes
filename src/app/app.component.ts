ebf982afd488f0b41147ab5e71ab63a22878b000
jnettles <jnettles@thoughtworks.com>
2017-02-08 16:11:52 -0500

    Used two way binding to get our change to be linked to our object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,10 @@ export class Hero {
     template: `
         <h1>{{title}}</h1>
         <h2>{{hero.name}} details!</h2>
-        <div><label>ID: </label>{{hero.id}}</div>
-        <div><label>Name: </label>{{hero.name}}</div>`,
+        <div>
+            <label>ID: </label>{{hero.id}}
+            <input [(ngModel)]='hero.name' placeholder="name">
+        </div>`
 })
 
 export class AppComponent {
